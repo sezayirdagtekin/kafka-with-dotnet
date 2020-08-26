cda959995eae695cfb0c916ba30def51a7c48b93
sezayirdagtekin <sezayir@hotmail.com>
2020-08-26 23:21:21 +0300

    implement kafka producer and consumer as adotnet Console

---
+++ KafkaConsumer/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+using System.Collections.Generic;
+using Confluent.Kafka;
+using System.IO;
+using System.Threading;
+
+namespace KafkaConsumer
+{
+    class Program
+    {
+        static string KAFKA_SERVER = "127.0.0.1:9092";
+        static string TOPIC = "weblog";
+        static string GROUP_ID = "testgroup";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kafka Consumer...!");
+            Console.WriteLine("address:" + KAFKA_SERVER);
+            Console.WriteLine("topicName:" + TOPIC);
+
+
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = KAFKA_SERVER,
+                GroupId = GROUP_ID,
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+            {
+                try
+                {
+
+                consumer.Subscribe(TOPIC);
+
+                CancellationTokenSource cts = new CancellationTokenSource();
+                Console.CancelKeyPress += (_, e) =>
+                {
+                    e.Cancel = true;
+                    cts.Cancel();
+                };
+                while(true){
+                          var cr = consumer.Consume(cts.Token);
+                            Console.WriteLine("Topic:"+ cr.Topic);
+                            Console.WriteLine("Partition:"+ cr.Partition);
+                            Console.WriteLine("Message key:"+ cr.Message.Key);  
+                             Console.WriteLine("Message Value:"+ cr.Message.Value);                          
+                            Thread.Sleep(500);
+                }
+                     
+            
+                    }
+                        
+                catch (Exception e)
+                {
+                    Console.WriteLine("Hata olustu" + e);
+                }
+
+            }
+
+        }
+
+    }
+}
---
+++ KafkaProducer/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using Confluent.Kafka;
+
+
+namespace KafkaProducer
+{
+    class Program
+    {
+        private const string KAFKA_SERVER = "localhost:9092";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("KafkaProducer is  starting");
+            sending();
+
+        }
+
+
+        private static void sending()
+        {
+            const string topic = "topic1";
+            Console.WriteLine("sending...");
+            var config = new ProducerConfig() { BootstrapServers = KAFKA_SERVER };
+
+            using (var producer = new ProducerBuilder<Null, string>(config).Build())
+            {
+                try
+                {
+                    int i = 1;
+                    while (i <= 5)
+                    {
+                        String text = "test msg  # " + i;
+                        DeliveryResult<Null, string> result = producer.ProduceAsync(topic, new Message<Null, string> { Value = text }).GetAwaiter().GetResult();
+                        Console.WriteLine($"Delivered to '{result.TopicPartitionOffset}'");
+                        i++;
+                    }
+               Console.WriteLine("Complete...");
+                }
+                catch (ProduceException<Null, string> err)
+                {
+                    Console.WriteLine("Hata olustu" + err);
+                }
+
+            }
+
+        }
+
+    }
+}
